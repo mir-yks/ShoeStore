a7d8cffdadca23bccae8707dd4031ed08d55ab9c
Mudrov Maksim <mudrov010203@gmail.com>
2025-12-19 23:32:28 +0300

    Верстка Register Account

--- app/src/main/java/com/example/shoestore/ui/components/DisableButton.kt
+++ app/src/main/java/com/example/shoestore/ui/components/DisableButton.kt
@@ -1,29 +1,62 @@
-package com.example.shoestore.ui.components
+package com.example.shoeshop.ui.components
 
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.shoestore.R
 
+
 @Composable
-fun DisableButton(modifier: Modifier = Modifier, text: String) {
-    Button(onClick = {}, shape = RoundedCornerShape(14.dp),
+fun DisableButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    backgroundColor: Color = colorResource(id = R.color.accent),
+    backgroundColordis: Color = colorResource(id = R.color.disable),
+    textColor: Color = Color.White
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        enabled = enabled,
         colors = ButtonDefaults.buttonColors(
-            containerColor = colorResource(R.color.disable),
-            contentColor = colorResource(R.color.background))) {
-        Text(text = text, fontSize = 14.sp)
+            containerColor = backgroundColor,
+            contentColor = textColor,
+            disabledContainerColor = backgroundColordis,
+            disabledContentColor = textColor
+        ),
+        shape = MaterialTheme.shapes.large,
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 0.dp
+        )
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Medium
+            )
+        )
     }
 }
 
 @Preview
 @Composable
 private fun DisableButtonPreview() {
-    DisableButton(text = "Текст кнопки")
+    DisableButton(
+        onClick = {  },
+        text = ""
+    )
 }
